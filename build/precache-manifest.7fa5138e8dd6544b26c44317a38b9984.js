9345308f5653773ed8d0e9d44bdfa9703b8bf724
wentao <569845499@qq.com>
2020-08-12 16:34:25 +0800

    uibuild

---
+++ build/precache-manifest.7fa5138e8dd6544b26c44317a38b9984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25764d9eff49e62a938f0c497f3d3231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ee55b723226ca8b6bf",
+    "url": "/static/js/2.f200ff29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f200ff29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d149c46409d41dc3e8f",
+    "url": "/static/js/main.c4818d27.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
